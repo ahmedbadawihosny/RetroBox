3ca92059feecd53b227fa70a8e87b6ca00e34e7d
ahmed badawi <ahmedbad063@gmail.com>
2024-12-18 18:20:17 +0200

    initial commit

---
+++ flutter/retro_box/android/app/src/main/kotlin/com/example/retro_box/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.retro_box
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter/retro_box/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // setupServiceLocator();
+  // await getIt<CacheHelper>().init();
+
+  runApp(
+    DevicePreview(
+      builder: (context) => MyApp(),
+      enabled: !kReleaseMode,
+    ),
+  );
+  // runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chattie',
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      // initialRoute: RoutesName.initial,
+      // onGenerateRoute: Pages.generate,
+      // home: GameScreen(),
+    );
+  }
+}
