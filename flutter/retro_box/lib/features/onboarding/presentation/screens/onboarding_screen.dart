3585e204f1c2e1542f34be20798206c8cb65e7ef
ahmed badawi <ahmedbad063@gmail.com>
2024-12-20 01:54:44 +0200

    refactor onboarding screen code

--- flutter/retro_box/lib/features/onboarding/presentation/screens/onboarding_screen.dart
+++ flutter/retro_box/lib/features/onboarding/presentation/screens/onboarding_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:retro_box/core/constants/app_colors.dart';
+import 'package:retro_box/core/routes/routes_names.dart';
 import 'package:retro_box/features/onboarding/data/data_source/onboarding_data.dart';
 import 'package:retro_box/features/onboarding/data/repository/onboarding_repository_impl.dart';
 import 'package:retro_box/features/onboarding/domain/entities/onboarding_entity.dart';
@@ -20,10 +21,8 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   final _controller = PageController();
   bool isLastPage = false;
-  double progressValue = 0.3333;
   int pageNum = 0;
 
-  // To hold the onboarding items
   List<OnboardingEntity> onboardingItems = [];
 
   @override
@@ -32,29 +31,34 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     _loadOnboardingData();
   }
 
-  /// Fetch data from the data layer using the repository and use case
   void _loadOnboardingData() {
     final repository = OnboardingRepositoryImpl(OnboardingData());
     final viewOnboarding = ViewOnboarding(repository);
 
     setState(() {
       onboardingItems = viewOnboarding();
-      progressValue = 1 / onboardingItems.length;
     });
   }
 
-  void _setOnboardingState() async {
+  void _setOnboardingState() {
     /*
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool('hasSeenOnboarding', true);
     */
-    if (!mounted) return;
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (_) => const Scaffold()));
+    // if (!mounted) return;
+    Navigator.pushReplacementNamed(
+      context,
+      RoutesName.xoHome,
+    );
   }
 
-  void goToNextPage() => _controller.nextPage(
-      duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
+  void goToNextPage() {
+    _controller.nextPage(
+        duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
+    if (isLastPage) {
+      _setOnboardingState();
+    }
+  }
 
   void goToLastPage() => _controller.previousPage(
       duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
@@ -84,11 +88,12 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     return SizedBox(
       height: 320,
       child: PageView.builder(
-        onPageChanged: (index) => setState(() {
-          isLastPage = onboardingItems.length - 1 == index;
-          pageNum = index;
-          progressValue = (index + 1) / onboardingItems.length;
-        }),
+        onPageChanged: (index) {
+          setState(() {
+            isLastPage = index >= onboardingItems.length - 1;
+            pageNum = index;
+          });
+        },
         controller: _controller,
         itemCount: onboardingItems.length,
         itemBuilder: (context, index) {
@@ -111,7 +116,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         const SizedBox(
           width: 25,
         ),
-        pageNum == 1 || pageNum == 2
+        pageNum > 0
             ? OnboardingBtn(
                 btnTitle: 'Back',
                 btnColor: AppColor.black,
@@ -134,15 +139,11 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               spacing: 3),
         ),
         const Spacer(),
-        isLastPage
-            ? OnboardingBtn(
-                btnTitle: 'Next',
-                btnColor: AppColor.black,
-                btnFun: _setOnboardingState)
-            : OnboardingBtn(
-                btnTitle: 'Next',
-                btnColor: AppColor.black,
-                btnFun: goToNextPage),
+        OnboardingBtn(
+          btnTitle: 'Next',
+          btnColor: AppColor.black,
+          btnFun: goToNextPage,
+        ),
         const SizedBox(
           width: 25,
         ),
