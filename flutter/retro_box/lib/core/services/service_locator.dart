73012bdfc74347e04b801a1547d184fc041b2a04
ahmed badawi <ahmedbad063@gmail.com>
2024-12-20 03:51:58 +0200

    refactor service_locator code

--- flutter/retro_box/lib/core/services/service_locator.dart
+++ flutter/retro_box/lib/core/services/service_locator.dart
@@ -1,8 +1,22 @@
 import 'package:get_it/get_it.dart';
 import 'package:retro_box/core/database/cache/cache_helper.dart';
+import 'package:retro_box/features/xo_game/data/data_soruce/remote_data_source.dart';
+import 'package:retro_box/features/xo_game/data/repository/xo_game_repository_impl.dart';
+import 'package:retro_box/features/xo_game/domain/repository/xo_game_repository.dart';
+import 'package:retro_box/features/xo_game/domain/usecases/make_ai_move.dart';
+import 'package:retro_box/features/xo_game/domain/usecases/reset_game.dart';
 
 final getIt = GetIt.instance;
+
 void setupServiceLocator() {
+  // Cahce Helper
   getIt.registerSingleton<CacheHelper>(CacheHelper());
-  // getIt.registerSingleton<AuthCubit>(AuthCubit());
+
+  // XO Game
+  getIt.registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImpl());
+  getIt.registerLazySingleton<XOGameRepository>(
+    () => XoGameRepositoryImpl(getIt<RemoteDataSource>()),
+  );
+  getIt.registerLazySingleton(() => MakeAIMove(repository: getIt<XOGameRepository>()));
+  getIt.registerLazySingleton(() => ResetGame(repository: getIt<XOGameRepository>()));
 }
